d32be5e2792ae5c822198ff2dd14f7ebc181e7ab
bruno-alves-startse <bruno.alves@startse.com>
2024-02-03 15:35:41 -0300

    fix: removed old file structure

--- src/presentation/components/core/Header/style.tsx
+++
@@ -1,7 +0,0 @@
-import { HTMLAttributes } from "react";
-
-export const container =
-  "z-20 h-full fixed top-0 w-full min-h-[80px] max-h-[80px] bg-white1000 flex justify-center items-center z-10 box-border border-b-[1px] border-b-gray400";
-
-export const contentNav =
-  "w-full max-w-[80rem] h-full flex flex-row justify-between items-center mx-2";
--- src/presentation/components/pages/Home/Skills/styles.ts
+++
@@ -1,2 +0,0 @@
-export const contentSlide =
-  "relative min-w-[100px] max-w-[100px] p-8 m-auto flex";
