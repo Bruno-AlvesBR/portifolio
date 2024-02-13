573e7a04ca812b72c969543f4c27b6a0132c9994
bruno-alves-startse <bruno.alves@startse.com>
2024-02-12 22:48:50 -0300

    fix: works historic

---
+++ src/components/pages/Home/Works/lists.ts
@@ -0,0 +1,42 @@
+import dayjs from "dayjs";
+
+export const worksList = [
+  {
+    id: "junior-developer-startse",
+    theme: "#55FFEE",
+    title: "Junior Full Stack Developer",
+    description: {
+      label:
+        "Acting as junior software developer from the company environment, I've participated of all processes of system performance and helpful customer experience and identification.",
+      lists: [
+        {
+          title: "Website's Performance Result",
+          description:
+            "System have increased of 40 points on Vercel Panel to 90 points of the company website performance",
+        },
+        {
+          title: "Users engagement",
+          description:
+            "Increased the users engagement (1 milion of access p/monthly).",
+        },
+        {
+          title: "Users identification",
+          description:
+            "Increased the users identification (0.5% to 4% of total actives).",
+        },
+        {
+          title: "Development Architecture",
+          description:
+            "Systems development using software tools with focus in performance and escalability.",
+        },
+      ],
+    },
+    company: {
+      name: "StartSe",
+      url: "https://www.startse.com",
+    },
+    startedAt: dayjs("02-10-2022"),
+    endedAt: dayjs(),
+    isActing: true,
+  },
+];
