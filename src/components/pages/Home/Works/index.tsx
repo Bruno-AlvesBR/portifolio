5a649ceddfba1e23b80a33ea0693dc786c1bf0e0
bruno-alvesbr <brunoph.faces12@gmail.com>
2024-05-25 00:52:24 -0300

    feat: update work data

--- src/components/pages/Home/Works/index.tsx
+++ src/components/pages/Home/Works/index.tsx
@@ -21,33 +21,31 @@ export const Works = () => {
           className={
             "sm:flex-col sm:justify-center sm:gap-6 sm:px-3 flex snap-start min-h-[100vh] w-[100vw] gap-32 px-16"
           }
+          style={{ background: work.color }}
         >
-          <div className="sm:py-0 flex flex-row gap-2 items-center justify-start backdrop-blur-sm border-white/10 border-[0.1px] bg-white/20 shadow-md rounded-lg p-2">
+          <div className="sm:py-0 flex flex-row gap-2 items-center justify-start backdrop-blur-sm border-white/10 border-[0.1px] bg-white/90 shadow-md rounded-lg p-2">
             <span className="sm:flex-row sm:w-full flex flex-col gap-0 items-center w-[120px]">
-              <p className="timeline-start text-white text-3 w-fit">
+              <p className="timeline-start text-black text-3 w-fit font-bold">
                 {formatWorkDate(work.startedAt.toISOString())}
               </p>
               <ChevronDownIcon className="sm:-rotate-90" />
-              <p className="timeline-start text-white text-3 w-fit">
+              <p className="timeline-start text-black text-3 w-fit font-bold">
                 {!work.isActing ? (
                   `${formatWorkDate(work.endedAt.toISOString())}`
                 ) : (
-                  <strong className="text-white">Acting now</strong>
+                  <strong className="text-black">Acting now</strong>
                 )}
               </p>
-              <p className="timeline-start w-fit text-white">
-                (
+              <p
+                className="timeline-start text-white px-2 rounded-md w-full text-center"
+                style={{ background: work.color }}
+              >
                 {workTime(
                   work.startedAt.toISOString(),
                   work.endedAt.toISOString()
                 )}
-                )
               </p>
             </span>
-
-            <div className="timeline-middle">
-              <VerifyIcon className="fill-white sm:hidden" />
-            </div>
           </div>
 
           <div className="sm:items-center sm:w-full md:px-4 sm:pr-2 sm:pl-2 sm:gap-4 timeline-end flex flex-col gap-10 mx-auto px-52">
@@ -83,13 +81,13 @@ export const Works = () => {
               {work.description.lists.map((label, index) => (
                 <li
                   key={Number(index)}
-                  className="collapse collapse-arrow bg-white/20 backdrop-blur-sm duration-150 transition-all hover:brightness-95"
+                  className="collapse collapse-arrow bg-white/70 backdrop-blur-sm duration-150 transition-all hover:brightness-95"
                 >
                   <input type="radio" name="my-accordion-2" />
-                  <div className="collapse-title sm:text-lg text-xl font-medium text-white">
+                  <div className="collapse-title sm:text-lg text-xl font-medium text-black">
                     {label.title}
                   </div>
-                  <div className="collapse-content text-white">
+                  <div className="collapse-content text-black">
                     <p>{label.description}</p>
                   </div>
                 </li>
--- src/components/pages/Home/Works/lists.ts
+++ src/components/pages/Home/Works/lists.ts
@@ -2,12 +2,17 @@ import dayjs from "dayjs";
 
 export const worksList = [
   {
-    id: "intern-developer-startse",
-    title: "Intern Full Stack Developer",
+    id: "developer-startse",
+    title: "Full Stack Developer",
     description: {
       label:
-        "Acting as intern software developer from the company environment, I've participated of all processes of system performance and helpful customer experience and identification.",
+        "Acting as software developer from the company environment, I've participated of all processes of system performance and helpful customer experience and identification.",
       lists: [
+        {
+          title: "Users identification",
+          description:
+            "Increased the users identification (0.5% to 4% of total actives).",
+        },
         {
           title: "Website's Performance Result",
           description:
@@ -17,28 +22,6 @@ export const worksList = [
           title: "Users engagement",
           description:
             "Increased the users engagement (1 milion of access p/monthly).",
-        },
-      ],
-    },
-    company: {
-      name: "StartSe",
-      url: "https://www.startse.com",
-    },
-    startedAt: dayjs("02-10-2022"),
-    endedAt: dayjs("12-31-2023"),
-    isActing: false,
-  },
-  {
-    id: "junior-developer-startse",
-    title: "Junior Full Stack Developer",
-    description: {
-      label:
-        "Acting as junior software developer from the company environment, I've participated of all processes of system performance and helpful customer experience and identification.",
-      lists: [
-        {
-          title: "Users identification",
-          description:
-            "Increased the users identification (0.5% to 4% of total actives).",
         },
         {
           title: "Development Architecture",
@@ -51,8 +34,32 @@ export const worksList = [
       name: "StartSe",
       url: "https://www.startse.com",
     },
-    startedAt: dayjs("01-01-2024"),
+    startedAt: dayjs("02-10-2022"),
+    endedAt: dayjs("05-04-2024"),
+    isActing: false,
+    color: "#0080ed",
+  },
+  {
+    id: "mid-developer-cidadania4u",
+    title: "Mid Full Stack Developer",
+    description: {
+      label:
+        "Acting as mid software developer from the company environment, I've participated of all processes of system performance and helpful customer experience and gamification.",
+      lists: [
+        {
+          title: "Internal users gamification",
+          description:
+            "Increase employees satisfaction with gamification techniques!",
+        },
+      ],
+    },
+    company: {
+      name: "Cidadania4u",
+      url: "https://www.cidadania4u.com.br",
+    },
+    startedAt: dayjs("04-15-2024"),
     endedAt: dayjs(),
     isActing: true,
+    color: "#630f2b",
   },
 ];
